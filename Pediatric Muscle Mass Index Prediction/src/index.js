e071d2650b8fa1b0c732208dc1fb1212fe011b49
Wasitpol Jongmanasjaroean <41641716+RewiKaze@users.noreply.github.com>
2021-04-12 20:31:45 +0700

    Update Front and all page

--- Pediatric Muscle Mass Index Prediction/src/index.js
+++ Pediatric Muscle Mass Index Prediction/src/index.js
@@ -1,15 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Calculator from "./pages/Calculator";
+import NavBar from "./components/NavBar";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/calc">
+          <Calculator />
+        </Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ Pediatric Muscle Mass Index Prediction/src/pages/Home.js
@@ -0,0 +1,26 @@
+import "../css/home.css";
+
+const Home = () => {
+  return (
+    <>
+      <div className="firstSection">
+        <div className="titleMain">
+          Body Composition and Muscle Mass Index<br></br>Analytic Program for
+          Children and Adult
+        </div>
+        <div className="descMain">
+          โปรแกรมวิเคราะห์องค์ประกอบร่างกายและดัชนีมวลกล้ามเนื้อสำหรับเด็กและผู้ใหญ่
+        </div>
+      </div>
+      <div className="secondSection"></div>
+      <div className="thirdSection">
+        <div className="boxWhite">
+          <div className="titleCalc">โปรแกรมคำนวณ</div>
+        </div>
+      </div>
+      <div className="footer">something that i don't know</div>
+    </>
+  );
+};
+
+export default Home;
